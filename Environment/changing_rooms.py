bdc193952ea370108d43d2094879bc4e8c209760
Michael Lam <michael.lam@student.ucll.be>
2025-12-15 16:11:42 +0100

    push push push Environment

--- Environment/changing_rooms.py
+++ Environment/changing_rooms.py
@@ -8,9 +8,11 @@ ROOMS_PATH = Path(__file__).parent.parent / "Rooms"
 rooms = []
 
 for image_path in ROOMS_PATH.glob("*"):
-    if image_path.suffix in (".avif", ".webp"):
+    if image_path.suffix in (".png"):
         image = pygame.image.load(image_path)
         rooms.append({
             "name": image_path.stem,
             "image": image
         })
+        
+print(rooms)
