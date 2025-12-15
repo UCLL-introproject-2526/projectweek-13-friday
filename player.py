24d05032bd495b84195a376043e4b0c310522bcd
Driss <driss@Drisss-MacBook-Air.local>
2025-12-15 16:55:24 +0100

    laatste versie

--- player.py
+++ player.py
@@ -1,23 +1,27 @@
 class Player:
-    def __init__(self,current_inventory, current_currency):
+    def __init__(self,current_inventory):
         self.inventory = current_inventory
-        self.currency = current_currency
+        self.current_currency = 0
+    def add_currency(self, currency):
+        self.current_currency += currency
         pass
 
+#make a player inventory that starts at 0 and can expand
 class Inventory:
     def __init__(self):
         self.current_inventory = {}
 
-    def add_to_inventory(self, type, q):
-        self.current_inventory[type] = q
+    def add_to_inventory(self, candy_type, q):
+        self.current_inventory[candy_type] = q
     def __repr__(self):
         return f'Inventory(\'{self.current_inventory})'
     
         
-
+#make an object for a candy type
 class Candy_type:
     def __init__(self,colour, flavour ):
-        pass
+        self.colour = colour
+        self.flavour = flavour
 
 
 bag = Inventory()
