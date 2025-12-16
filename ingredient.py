38a40ab95e1e9154cba85ef700c8715cf41782a1
Aagje Juste <aagje.juste@student.ucll.be>
2025-12-16 19:57:54 +0100

    ingredient

--- ingredient.py
+++ ingredient.py
@@ -4,3 +4,24 @@ class Ingredient:
 
     def __repr__(self):
         return self.name
+    
+
+    def click_function(self):
+        pass
+    
+    click = set()
+
+    def check_recipe(self):
+
+
+        recipe = {  snoep1: {"blauw", "appel"}, snoep2: {"blauw", "banaan"}, snoep3: {"blauw", "druif"}, 
+            snoep4: {"geel", "appel"}, snoep5: {"geel", "banaan"}, snoep6: {"geel", "druif"},
+            snoep7: {"rood", "appel"}, snoep8: {"rood", "banaan"}, snoep9: {"rood", "druif"}}
+        
+        for snoep in recipe:
+            pass
+    
+
+
+
+
